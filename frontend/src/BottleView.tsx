40b02eda91bd0d1160477bbf532a5e5cd340207f
Simon Blum <simonblum@hotmail.de>
2025-11-04 16:28:15 +0100

    responsive DetailView

--- frontend/src/BottleView.tsx
+++ frontend/src/BottleView.tsx
@@ -13,25 +13,24 @@ export const BottleView = ({articles}: BottleViewProps) => {
             gridTemplateColumns: 'repeat(auto-fill, minmax(160px, 1fr))',
         }}
         >
-        {articles.map(a => <Box
-        sx={{
-            aspectRatio: '1 / 1',
-            borderRadius: 2,
-            bgcolor: 'grey.100',
-            overflow: 'hidden',
-            p: 1,
-          }}
-        key={a.id}>
-        <Box
-        
-        component="img"
-        src={a.image}
-        sx={{
-            width: '100%',
-            height: '100%',
-            objectFit: 'contain',
-            display: 'block'
-    }} />
-    </Box>)}
+            {articles.map(a => <Box
+            sx={{
+                aspectRatio: '1 / 1',
+                borderRadius: 2,
+                bgcolor: 'grey.100',
+                overflow: 'hidden',
+                p: 1,
+            }}
+            key={a.id}>
+                <Box
+                component="img"
+                src={a.image}
+                sx={{
+                    width: '100%',
+                    height: '100%',
+                    objectFit: 'contain',
+                    display: 'block'
+                }} />
+            </Box>)}
     </Box>;
 }
--- frontend/src/DetailView.tsx
+++ frontend/src/DetailView.tsx
@@ -1,4 +1,4 @@
-import { Card } from "@mui/material";
+import { Box, Card, CardContent, Stack, Typography } from "@mui/material";
 import type { Article } from "./article.types";
 
 type DetailViewProps = {
@@ -7,6 +7,42 @@ type DetailViewProps = {
 
 export const DetailView = ({articles}: DetailViewProps) => {
     return <>{articles.map(a => <Card key={a.id}>
-        <img key={a.id} src={a.image}/>
+        <CardContent>
+            <Stack direction="row" spacing={2} alignItems="flex-start">
+              <Box
+                sx={{
+                  width: { xs: 96, sm: 120, md: 140 },
+                  aspectRatio: '1 / 1',
+                  flex: '0 0 auto',
+                  borderRadius: 2,
+                  bgcolor: 'grey.100',
+                  overflow: 'hidden',
+                }}
+              >
+                <Box
+                  component="img"
+                  src={a.image}
+                  alt={a.name}
+                  loading="lazy"
+                  decoding="async"
+                  sx={{
+                    width: '100%',
+                    height: '100%',
+                    objectFit: 'contain',
+                    display: 'block',
+                  }}
+                />
+              </Box>
+              <Box sx={{ flex: 1, minWidth: 0 }}>
+                <Typography variant="subtitle1" noWrap>{a.name}</Typography>
+                <Typography variant="body2" color="text.secondary" sx={{ mb: 0.5 }}>
+                  {a.price}
+                </Typography>
+                <Typography variant="body2">
+                  {a.shortDescription}
+                </Typography>
+              </Box>
+            </Stack>
+        </CardContent>
     </Card>)}</>;
 }
