c407cf06b868f075bffad44c086e413f71625a0d
Simon Blum <simonblum@hotmail.de>
2025-11-04 15:24:03 +0100

    fetch articles

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import flapoLogo from '/favicon.png'
 import './App.css'
 import { AppBar, Box, Button, Stack, Toolbar } from '@mui/material'
@@ -6,24 +6,48 @@ import FilterAltIcon from '@mui/icons-material/FilterAlt';
 import FilterAltOffIcon from '@mui/icons-material/FilterAltOff';
 import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward';
 import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward';
+import type { Article } from './article.types';
+
+const API_URL = 'https://localhost:7092/article'
 
 function App() {
   const [sort, setSort] = useState<string>('');
   const [bottleView, setBottleView] = useState<boolean>(false);
   const [filter, setFilter] = useState<boolean>(false);
 
+  const [articles, setArticles] = useState<Article[]>();
+
+  useEffect(() => {
+    const params = new URLSearchParams();
+    if (sort !== ''){
+      params.append('sortByPrice', sort);
+    }
+    if (filter){
+      params.append('filter', String(filter));
+    }
+
+    const fetchArticles = async () => {
+      const response = await fetch(`${API_URL}?${params}`);
+      const articles = await response.json() as Article[];
+      setArticles(articles);
+    }
+    fetchArticles();
+  }, [sort, filter]);
+
   const toggleView = () => setBottleView(p => !p);
   const toggleFilter = () => setFilter(p => !p);
   const toggleSort = () => setSort(p => {
     if (!p){
       return 'asc';
-    }
-    if (p === 'asc'){
+    } else if (p === 'asc'){
       return 'desc';
     } else {
       return '';
     }
   })
+
+  console.log('articles: ', articles);
+  
 
   return (
     <>
---
+++ frontend/src/article.types.ts
@@ -0,0 +1,7 @@
+export type Article = {
+    id: number;
+    name: string;
+    shortDescription: string;
+    price: number;
+    image: string;
+}
